08bb3f208bffb331bb1ffcb49db1062f03e7753e
aashi1303 <168280808+aashi1303@users.noreply.github.com>
2024-04-27 19:41:25 +0530

    final upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('body').openAccessibility({
+        textSelector: 'div,span,p,a,td,table,tr',
+        isMenuOpened: false,
+        iconSize: 'm',
+        localization: ['en'],
+    });
+})
+
