fd8a2a965cfc47825acabdbc112e2e18a5a36165
aashi1303 <168280808+aashi1303@users.noreply.github.com>
2024-04-27 19:40:04 +0530

    Delete open-accessibility-master directory

--- open-accessibility-master/script.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $('body').openAccessibility({
-        textSelector: 'div,span,p,a,td,table,tr',
-        isMenuOpened: false,
-        iconSize: 'm',
-        localization: ['en'],
-    });
-})
-
